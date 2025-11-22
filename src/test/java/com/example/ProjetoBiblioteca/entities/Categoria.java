6bd9a5f1f794af3d455d3f9bb5911a6538918ede
mviscardi3 <miguelviscardi@gmail.com>
2025-11-22 16:47:34 -0300

    Delete src directory

--- src/test/java/com/example/ProjetoBiblioteca/entities/Categoria.java
+++
@@ -1,7 +0,0 @@
-package com.example.ProjetoBiblioteca.entities;
-
-public class Categoria {
-    private String nome;
-    private String descicao;
-    private String livro;
-}
--- src/test/java/com/example/ProjetoBiblioteca/entities/Editora.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProjetoBiblioteca.entities;
-
-public class Editora {
-    private String Nome;
-    private String cnpj;
-
-    private String endereco;
-
-}
